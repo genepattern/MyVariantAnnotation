4e37c1720e122db4588bcbcf36872c5ae8d979e3
Alex <atwenzel@eng.ucsd.edu>
2018-08-23 16:52:57 -0700

    added function for loading variants

--- src/Utils.py
+++ src/Utils.py
@@ -3,6 +3,8 @@ This script contains utilities for processing data
 before/after annotation
 """
 
+import pandas as pd
+
 """
 Variant Formatting
 """
@@ -41,3 +43,37 @@ def parse_delins(chrom, pos, ref, alt):
     pos = int(pos)
     pstr = str(pos)+"_"+str(pos+len(ref)-1)
     return chrom+":g."+pstr+"delins"+alt
+
+"""
+Flatten JSON
+"""
+
+def json2df(jd):
+    """
+    Takes a JSON object and calls json_normalize. Remaining list elements
+    are converted to comma-separated strings
+    """
+    df = pd.io.json.json_normalize(jd)
+    for col in df.columns:
+        df[col] = df[col].apply(lambda x: ','.join(list(map(str, x))) if type(x) == list else x)
+    return df
+
+"""
+Read Input
+"""
+
+def load_variants(path):
+    """
+    Loads a list of tuples of variants from a path. Variants
+    should come from a tab-separated file with at minimum column
+    headers "chromosome", "position", "reference", "alternate"
+
+        path (str): The path to a variant input file
+
+        return ([(str, str, str, st)]): The variant tuples
+    """
+    variants = []
+    df = pd.read_csv(path, sep='\t')
+    for i, row in df.iterrows():
+        variants.append((row['chromosome'], str(row['position']), row['reference'], row['alternate']))
+    return variants
