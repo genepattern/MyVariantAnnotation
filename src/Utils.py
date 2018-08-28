7c99c62449ff19dcbef3b96944bde6cc55cb3d58
Alex <atwenzel@eng.ucsd.edu>
2018-08-28 12:27:52 -0700

    finished list/dict parsing functions

--- src/Utils.py
+++ src/Utils.py
@@ -3,6 +3,7 @@ This script contains utilities for processing data
 before/after annotation
 """
 
+import numpy as np
 import pandas as pd
 
 """
@@ -48,6 +49,61 @@ def parse_delins(chrom, pos, ref, alt):
 Flatten JSON
 """
 
+def parse_dict(d):
+    """
+    Given a single layer dictionary d, return a string representation
+    of the form "key1=val1,key2=val2,..."
+
+        d (dict): Any dictionary, preferrably with no nesting
+
+        returns (str): String represenation of d described above
+    """
+    return ",".join([str(key)+"="+str(value) for key, value in d.iteritems()])
+
+def parse_dict_list(dl):
+    """
+    Returns the string representation of a list of dictionaries in
+    the form "key1=val1,key2=val2|key1=val2,..."
+
+        dl ([dict]): List of dictionaries
+
+        returns (str): String representation of dl described above
+    """
+    return "|".join([parse_dict(d) for d in dl])
+
+def is_list_of_dict(l):
+    """
+    Checks if a list is entirely composed of dictionaries
+
+        l ([]): List of any type
+
+        returns (bool): True if l is entirely composed of dicts
+    """
+    return all(type(x) == dict for x in l)
+
+def parse_lists(elem):
+    """
+    Takes a single element from a column in a json_normalize()
+    result and parse the element if it is a list of values or
+    a list of dictionaries. List of dictionaries are converted
+    to strings of the form "key1=val1,key2=val2|..." and all other
+    lists are converted to "val1|val2|...". Non-list elements are
+    left unchanged.
+
+        elem (any): Column value from a json_normalize() call
+
+        returns (str): Parsed version of the element
+    """
+    if type(elem) == list:
+        if is_list_of_dict(elem):
+            return parse_dict_list(elem)
+        else:
+            return "|".join(list(map(str, elem)))
+    elif pd.isnull(elem):
+        return elem
+    else:
+        return str(elem)
+
 def json2df(jd):
     """
     Takes a JSON object and calls json_normalize. Remaining list elements
@@ -55,8 +111,13 @@ def json2df(jd):
     """
     df = pd.io.json.json_normalize(jd)
     for col in df.columns:
-        df[col] = df[col].apply(lambda x: ','.join(list(map(str, x))) if type(x) == list else x)
-    return df
+        df[col] = df[col].apply(parse_lists)
+    ## dbnsfp parsing steps
+    for i, row in df.iterrows():
+        if not pd.isnull(row['dbnsfp.aa']):
+            row['dbnsfp.aa.refcodon'] = '|'.join([rc.split('=')[-1] 
+                                                    for rc in row['dbnsfp.aa'].split('|')])
+    return df[df.columns[::-1]]
 
 """
 Read Input
@@ -70,7 +131,7 @@ def load_variants(path):
 
         path (str): The path to a variant input file
 
-        return ([(str, str, str, st)]): The variant tuples
+        return ([(str, str, str, str)]): The variant tuples
     """
     variants = []
     df = pd.read_csv(path, sep='\t')
